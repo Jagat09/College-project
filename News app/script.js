c17095d8e731b065bb0e5b4384eec8335a9617b7
Jagat Sharma <85271961+Jagat09@users.noreply.github.com>
2023-06-05 19:38:51 +0530

    Update script.js
    api change

--- News app/script.js
+++ News app/script.js
@@ -1,4 +1,4 @@
-const API_KEY = "1d3a0eefa97b499d8fbc4ee93eeb40b7";
+const API_KEY = "d9baf419ab214174b044d849aa85062d";
 const url = "https://newsapi.org/v2/everything?q=";
 
 window.addEventListener("load", () => fetchNews("India"));
